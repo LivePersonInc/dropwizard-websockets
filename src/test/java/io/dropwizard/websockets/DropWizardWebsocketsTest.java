25ba51ff22ca94074082402f49a27066873bc39a
eitan101 <eitanya@liveperson.com>
2016-08-30 12:44:08 +0300

    -

--- src/test/java/io/dropwizard/websockets/DropWizardWebsocketsTest.java
+++ src/test/java/io/dropwizard/websockets/DropWizardWebsocketsTest.java
@@ -55,8 +55,10 @@ public class DropWizardWebsocketsTest {
         serverThread.setDaemon(true);
         serverThread.start();
         serverStarted.await(10, SECONDS);
-        if (System.getProperty(TRAVIS_ENV)!=null)
-            Thread.sleep(10); // Ugly sleep to debug travis
+        if (System.getProperty(TRAVIS_ENV)!=null) {
+            System.out.println("waiting for Travis machine");
+            Thread.sleep(10); // Ugly sleep to debug travis            
+        }
     }
     private static final String TRAVIS_ENV = "TRAVIS_ENV";
     private CloseableHttpClient client;
