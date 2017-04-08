7813942b998c7e0a2aa583c908051f99daff769b
Alex Katlein <dev@vemilyus.com>
2017-04-08 09:32:49 +0200

    Fixed to enable use with Dropwizard 1.1.0
    Updated jetty version to match Dropwizard 1.1.0
    Bumped version to match Dropwizard

--- src/main/java/io/dropwizard/metrics/jetty9/websockets/InstWebSocketServerContainerInitializer.java
+++ src/main/java/io/dropwizard/metrics/jetty9/websockets/InstWebSocketServerContainerInitializer.java
@@ -29,19 +29,23 @@ import io.dropwizard.metrics.jetty9.websockets.endpoint.InstJsrServerExtendsEndp
 import javax.servlet.ServletException;
 import org.eclipse.jetty.websocket.common.events.EventDriverFactory;
 import org.eclipse.jetty.websocket.jsr356.server.ServerContainer;
+import org.eclipse.jetty.websocket.server.NativeWebSocketConfiguration;
 import org.eclipse.jetty.websocket.server.WebSocketUpgradeFilter;
 
 public class InstWebSocketServerContainerInitializer {
     public static ServerContainer configureContext(final MutableServletContextHandler context, final MetricRegistry metrics) throws ServletException {
         WebSocketUpgradeFilter filter = WebSocketUpgradeFilter.configureContext(context);
-        ServerContainer wsContainer = new ServerContainer(filter, filter.getFactory(), context.getServer().getThreadPool());
-        EventDriverFactory edf = filter.getFactory().getEventDriverFactory();
+        NativeWebSocketConfiguration wsConfig = filter.getConfiguration();
+        
+        ServerContainer wsContainer = new ServerContainer(wsConfig, context.getServer().getThreadPool());
+        EventDriverFactory edf = wsConfig.getFactory().getEventDriverFactory();
         edf.clearImplementations();
 
         edf.addImplementation(new InstJsrServerEndpointImpl(metrics));
         edf.addImplementation(new InstJsrServerExtendsEndpointImpl(metrics));
         context.addBean(wsContainer);
         context.setAttribute(javax.websocket.server.ServerContainer.class.getName(), wsContainer);
+        context.setAttribute(WebSocketUpgradeFilter.class.getName(), filter);
         return wsContainer;
     }
 }
